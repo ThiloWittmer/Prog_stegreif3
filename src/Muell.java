50e9335788bb09dd514e9d2d5747d746508e13dc
muhanad <muhanadkhatib998@gmail.com>
2022-06-29 17:09:02 +0200

    Taucher/Muell-kalssen änderungen

---
+++ src/Muell.java
@@ -0,0 +1,35 @@
+
+public class Muell implements Leckerbissen {
+    private boolean lebendig;
+    private final Nahrungstyp TYP= Nahrungstyp.NICHT_ESSBAR;
+    private int gewicht;
+    
+    public Muell(boolean lebendig, int gewicht){
+        this.lebendig= true;
+        this.gewicht= gewicht;
+    }
+
+    @Override
+    public int getGramm() {
+    
+        return gewicht;
+    }
+
+    @Override
+    public boolean gefressen() {
+        lebendig = false;
+		return true;
+    }
+
+    @Override
+    public boolean istLebendig() {
+        lebendig = false;
+		return lebendig;
+    }
+
+    @Override
+    public Nahrungstyp getNahrungstyp() {
+        return TYP;
+    }
+
+}
---
+++ src/Taucher.java
@@ -0,0 +1,41 @@
+
+public class Taucher implements Leckerbissen {
+    private boolean lebendig;
+    private final Nahrungstyp TYP = Nahrungstyp.FLEISCH; 
+    private int gewicht;
+
+    public Taucher(boolean lebendig, int gewicht){
+        this.lebendig=true;
+        this.gewicht= gewicht;
+    }
+
+    @Override
+    public int getGramm() {
+    
+        return gewicht;
+    }
+
+    @Override
+    public boolean gefressen() {
+        if(lebendig){
+            lebendig = false;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean istLebendig() {
+        
+        return lebendig;
+    }
+
+    @Override
+    public Nahrungstyp getNahrungstyp() {
+    
+        return TYP;
+    }
+
+
+
+}
